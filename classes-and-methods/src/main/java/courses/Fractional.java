d982508eceb15d635255341babd8125028f53201
Anatoly Khalak <inno0747@gmail.com>
2022-11-28 00:05:19 +0300

    Вывод только дробной части числа x

---
+++ classes-and-methods/src/main/java/courses/Fractional.java
@@ -0,0 +1,23 @@
+package courses;
+
+public class Fractional {
+    public static void main(String[] args){
+        System.out.println(fraction(5.3));
+    }
+    public static double fraction(double x){
+        int res = (int)x;
+        double result = x - res;
+        return result;
+    }
+}
+
+/*
+Классы и методы #1
+Дробная часть. Дана следующая сигнатура метода:
+public static double fraction(double x);
+Необходимо реализовать метод таким образом, чтобы он возвращал только дробную часть числа х.
+Подсказка: вещественное число может быть преобразовано к целому путем отбрасывания дробной части.
+Пример:
+x=5,3
+результат: 0,3
+ */
