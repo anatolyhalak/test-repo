052db93bc73b31a781ba4ee8ee5be479abffa110
Roman <javadev442@gmail.com>
2023-03-14 22:10:54 +0300

    5 oop answer

--- classes-and-methods/src/main/java/ru/courses/oop/encapsulation/sotrudniki_4/MainApp.java
+++ classes-and-methods/src/main/java/ru/courses/oop/encapsulation/sotrudniki_4/MainApp.java
@@ -11,4 +11,82 @@ public class MainApp {
         System.out.println(ivan);
         System.out.println(vasya);
     }
+
+
+    public static class Otdel {
+        private String name;
+        private Sotrudnik chief;
+
+        public Otdel(String name) {
+            this.name = name;
+        }
+
+        public Otdel(String name, Sotrudnik chief) {
+            this.name = name;
+            this.chief = chief;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Sotrudnik getChief() {
+            return chief;
+        }
+
+        public void setChief(Sotrudnik chief) {
+            this.chief = chief;
+        }
+
+        @Override
+        public String toString() {
+            return "Отдел " + name + ", Начальник отдела " + chief;
+        }
+    }
+
+
+    public static class Sotrudnik {
+        private String name;
+        private Otdel otdel;
+
+        public Sotrudnik(String name) {
+            this.name = name;
+        }
+
+        public Sotrudnik(String name, Otdel otdel) {
+            this.name = name;
+            this.otdel = otdel;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Otdel getOtdel() {
+            return otdel;
+        }
+
+        public void setOtdel(Otdel otdel) {
+            this.otdel = otdel;
+        }
+
+        @Override
+        public String toString() {
+            if (otdel.getChief().name.equals(this.name)) {
+                return name + " Начальник отдела " + otdel.getName();
+            } else {
+                return name + " работает в отделе " + otdel.getName();
+            }
+        }
+    }
+
+
 }
